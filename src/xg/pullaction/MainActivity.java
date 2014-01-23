ebaeb6d595e8e2dd9a8c8ef3925a793ed096d81a
androidzhaoxiaogang <18008021570@189.cn>
2014-01-23 16:14:34 +0800

    home page implementation;

--- src/xg/pullaction/MainActivity.java
+++ src/xg/pullaction/MainActivity.java
@@ -1,22 +1,39 @@
 package xg.pullaction;
 
+
+import xg.pullaction.adapters.MyPagerAdapter;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.astuetz.PagerSlidingTabStrip;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockFragmentActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle paramBundle) {
+		super.onCreate(paramBundle);
 		setContentView(R.layout.activity_main);
+		
+		PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+		
+		tabs.setIndicatorColorResource(R.color.tab_indicator);
+		ViewPager pager = (ViewPager) findViewById(R.id.pager);
+		pager.setOffscreenPageLimit(5);
+		MyPagerAdapter adapter = new MyPagerAdapter(this, getSupportFragmentManager());
+		
+		final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+				8, getResources().getDisplayMetrics());
+		pager.setPageMargin(pageMargin);
+		pager.setAdapter(adapter);
+		tabs.setViewPager(pager);
 	}
-
+	
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
-
 }
---
+++ src/xg/pullaction/adapters/MyPagerAdapter.java
@@ -0,0 +1,51 @@
+package xg.pullaction.adapters;
+
+import xg.pullaction.R;
+import xg.pullaction.fragments.HomeFragment;
+import xg.pullaction.fragments.HomeFragment1;
+import xg.pullaction.fragments.HomeFragment2;
+import xg.pullaction.fragments.HomeFragment3;
+import xg.pullaction.fragments.HomeFragment4;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+	private String[] TITLES;
+
+	public MyPagerAdapter(Context context,
+			android.support.v4.app.FragmentManager fm) {
+		super(fm);
+		TITLES = context.getResources().getStringArray(R.array.fragment_title);
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return TITLES[position];
+	}
+
+	@Override
+	public int getCount() {
+		return TITLES.length;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		switch (position) {
+		case 0:
+			return HomeFragment.newInstance(position);
+		case 1:
+			return HomeFragment1.newInstance(position);
+		case 2:
+			return HomeFragment2.newInstance(position);
+		case 3:
+			return HomeFragment3.newInstance(position);
+		case 4:
+			return HomeFragment4.newInstance(position);
+		default :
+			return HomeFragment.newInstance(position);
+		
+		}
+	}
+
+}
